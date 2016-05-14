5556833fbca226c1530e410553aabe3ab232f759
Sathish G <sathish.gajendran@gmail.com>
2016-05-14 09:26:32 +0530

    test commit

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>AngularAttack 2016</h1><p>Your project runs!</p>'
+      template: '<h1>AngularAttack 2016</h1><p>Hello World...</p>'
     })
     .Class({
       constructor: function() {}
